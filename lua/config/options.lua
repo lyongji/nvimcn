f5f0df1e0486587f759152392ac63d50e45189fd
LYJ <532874387@qq.com>
2025-06-16 13:30:21 +0800

    配置换行和拼写

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+
+local opt = vim.opt
+
+opt.wrap = true
+opt.spelllang = { "en", "cjk" } -- 拼写检查 中文 英文
+opt.linebreak = false -- 更适合中文换行
+
+vim.g.root_spec = { "cwd" }
